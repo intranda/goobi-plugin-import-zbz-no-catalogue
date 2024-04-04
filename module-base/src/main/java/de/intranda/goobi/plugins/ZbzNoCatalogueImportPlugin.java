529bc62b5eb7a37fe3c908ced66e362444f3e57d
Steffen Hankiewicz <steffen.hankiewicz@intranda.com>
2024-04-04 07:59:11 +0200

    initial commit

--- module-base/src/main/java/de/intranda/goobi/plugins/ZbzNoCatalogueImportPlugin.java
+++ module-base/src/main/java/de/intranda/goobi/plugins/ZbzNoCatalogueImportPlugin.java
@@ -42,10 +42,10 @@ import ugh.fileformats.mets.MetsMods;
 
 @PluginImplementation
 @Log4j2
-public class KickStartImportPlugin implements IImportPluginVersion2 {
+public class ZbzNoCatalogueImportPlugin implements IImportPluginVersion2 {
 
     @Getter
-    private String title = "intranda_import_kick_start";
+    private String title = "intranda_import_zbz_no_catalogue";
     @Getter
     private PluginType type = PluginType.Import;
 
@@ -78,7 +78,7 @@ public class KickStartImportPlugin implements IImportPluginVersion2 {
     /**
      * define what kind of import plugin this is
      */
-    public KickStartImportPlugin() {
+    public ZbzNoCatalogueImportPlugin() {
         importTypes = new ArrayList<>();
         importTypes.add(ImportType.FILE);
     }
@@ -249,7 +249,7 @@ public class KickStartImportPlugin implements IImportPluginVersion2 {
                 fileformat.write(fileName);
                 io.setImportReturnValue(ImportReturnValue.ExportFinished);
             } catch (UGHException e) {
-                log.error("Error while creating Goobi processes in the KickStartImportPlugin", e);
+                log.error("Error while creating Goobi processes in the ZbzNoCatalogueImportPlugin", e);
                 io.setImportReturnValue(ImportReturnValue.WriteError);
             }
 
